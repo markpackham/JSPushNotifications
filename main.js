39907eed41955097d9a62adf0cc8d9a0455b4c92
mark <markpackham1@gmail.com>
2022-09-12 14:53:00 +0100

    demo use of notification data
    https://youtu.be/Bm0JjR4kP8w?t=271

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
   Notification.requestPermission().then((perm) => {
-    alert(perm);
+    if (perm === "granted") {
+      const notification = new Notification("Example notification", {
+        body: "This is body text",
+        data: { hello: "world" },
+      });
+
+      console.log(notification);
+
+      notification.data.addEventListener("close", (e) => {
+        console.log(e);
+      });
+    }
   });
 });
